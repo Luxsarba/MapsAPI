2359ed1c083cace03aa56d4b473dfa365fd0a675
Luxsarba <luxarba@yandex.ru>
2025-03-18 20:36:55 +0500

    press pg dn,pg up for zoom

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-import sys
 import os
 from dotenv import load_dotenv
 import requests
-from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
-from PyQt6.QtGui import QPixmap
 from io import BytesIO
+import sys
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
 
-print("Загружаю .env:", os.path.exists("../YandexMapsAPI/.env"))
 load_dotenv()
-
 API_KEY = os.getenv("STATIC_MAPS_API_KEY")
+
 COORDS = "37.6173,55.7558"  # Москва
-ZOOM = "10"
+ZOOM = 10
+MIN_ZOOM = 1
+MAX_ZOOM = 18
 
 
 class MapApp(QMainWindow):
@@ -20,13 +22,16 @@ class MapApp(QMainWindow):
         self.setWindowTitle("Yandex Maps Viewer")
         self.setGeometry(100, 100, 600, 450)
 
-        map_data = self.get_map(COORDS, ZOOM)
+        self.label = QLabel(self)
+        self.label.setGeometry(0, 0, 600, 450)
 
-        label = QLabel(self)
+        self.update_map()
+
+    def update_map(self):
+        map_data = self.get_map(COORDS, ZOOM)
         pixmap = QPixmap()
         pixmap.loadFromData(map_data)
-        label.setPixmap(pixmap)
-        label.setGeometry(0, 0, 600, 450)
+        self.label.setPixmap(pixmap)
 
     def get_map(self, coords, zoom):
         url = f"https://static-maps.yandex.ru/1.x/?ll={coords}&z={zoom}&size=600,450&l=map&apikey={API_KEY}"
@@ -36,6 +41,14 @@ class MapApp(QMainWindow):
         else:
             print("Ошибка загрузки карты")
             sys.exit(1)
+
+    def keyPressEvent(self, event):
+        global ZOOM
+        if event.key() == Qt.Key.Key_PageUp and ZOOM < MAX_ZOOM:
+            ZOOM += 1
+        elif event.key() == Qt.Key.Key_PageDown and ZOOM > MIN_ZOOM:
+            ZOOM -= 1
+        self.update_map()
 
 
 if __name__ == "__main__":
