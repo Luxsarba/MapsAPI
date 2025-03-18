57b32a896d8d0406e7836c944d647e551ec5c78e
Luxsarba <luxarba@yandex.ru>
2025-03-18 21:01:17 +0500

    requesting by params

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ COORDS = [37.6173, 55.7558]  # Москва (долгота, широта)
 ZOOM = 10
 MIN_ZOOM = 1
 MAX_ZOOM = 17
-MOVE_STEP = 0.1
+
+
+def get_move_step(zoom):  # Шаг перемещения в градусах (зависит от масштаба)
+    return 180 / (2 ** zoom)
 
 
 class MapApp(QMainWindow):
@@ -35,8 +38,15 @@ class MapApp(QMainWindow):
         self.label.setPixmap(pixmap)
 
     def get_map(self, coords, zoom):
-        url = f"https://static-maps.yandex.ru/1.x/?ll={coords[0]},{coords[1]}&z={zoom}&size=600,450&l=map&apikey={API_KEY}"
-        response = requests.get(url)
+        url = "https://static-maps.yandex.ru/1.x/"
+        params = {
+            "ll": f"{coords[0]},{coords[1]}",
+            "z": zoom,
+            "size": "600,450",
+            "l": "map",
+            "apikey": API_KEY
+        }
+        response = requests.get(url, params=params)
         if response.status_code == 200:
             return BytesIO(response.content).read()
         else:
@@ -45,18 +55,19 @@ class MapApp(QMainWindow):
 
     def keyPressEvent(self, event):
         global ZOOM, COORDS
+        move_step = get_move_step(ZOOM)
         if event.key() == Qt.Key.Key_PageUp and ZOOM < MAX_ZOOM:
             ZOOM += 1
         elif event.key() == Qt.Key.Key_PageDown and ZOOM > MIN_ZOOM:
             ZOOM -= 1
         elif event.key() == Qt.Key.Key_Up:
-            COORDS[1] = min(COORDS[1] + MOVE_STEP, 85.0)
+            COORDS[1] = min(COORDS[1] + move_step, 85.0)
         elif event.key() == Qt.Key.Key_Down:
-            COORDS[1] = max(COORDS[1] - MOVE_STEP, -85.0)
+            COORDS[1] = max(COORDS[1] - move_step, -85.0)
         elif event.key() == Qt.Key.Key_Left:
-            COORDS[0] = max(COORDS[0] - MOVE_STEP, -180.0)
+            COORDS[0] = max(COORDS[0] - move_step, -180.0)
         elif event.key() == Qt.Key.Key_Right:
-            COORDS[0] = min(COORDS[0] + MOVE_STEP, 180.0)
+            COORDS[0] = min(COORDS[0] + move_step, 180.0)
         self.update_map()
 
 
